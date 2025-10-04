abc817a63d9016a18bb2fb5b0d177608802b8e6a
Leonhard Reichenbach <leonhard.reichenbach@cern.ch>
2025-10-04 16:50:43 +0200

    fix copy paste mistake

--- ReweightingHelper.py
+++ ReweightingHelper.py
@@ -58,7 +58,7 @@ class ReweightingHelper(Analysis):
     def define_optimal_observables_reco(self, names: list[str]):
         for name in names:
             var = AltSetupHandler.get_var_from_name_1d(name)
-            self._define((f"mc_O_{name}", f"{1/var} * (mc_sqme_{name} - mc_sqme_nominal) / mc_sqme_nominal"), truth_categories)
+            self.Define(f"O_{name}", f"{1/var} * ((reco_sqme_12_{name} + reco_sqme_21_{name}) - (reco_sqme_12_nominal + reco_sqme_21_nominal)) / (reco_sqme_12_nominal + reco_sqme_21_nominal)")
 
 
     # TODO: some kind of prefix etc. would be good to add everywhere when the variations also come in
